0989faaba01473ebc1785cc3d2305f0ea4f099fd
andressa-leite <dre_tl@hotmail.com>
2025-10-30 15:41:44 -0300

    Added task separator

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,8 @@
 import { Button } from './Button';
 import { MdAdd } from 'react-icons/md';
 import { VscTrash } from 'react-icons/vsc';
+import { LuCloudSun, LuSun } from 'react-icons/lu';
+import { GrMoon } from 'react-icons/gr';
 
 function Tasks() {
   return (
@@ -10,6 +12,7 @@ function Tasks() {
           <span className="text-xs font-semibold text-[#00ADB5]">My Tasks</span>
           <h2 className="text-xl font-semibold">My Tasks</h2>
         </div>
+
         <div className="flex items-center gap-3">
           <Button variant="ghost">
             Limpar Tarefas
@@ -19,6 +22,37 @@ function Tasks() {
             Nova Tarefa
             <MdAdd />
           </Button>
+        </div>
+      </div>
+      <div className="rounded-xl bg-white pl-5 p-1">
+        <div className="my-6 space-y-3">
+          {/* Morning */}
+
+          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
+            <p className="pt-1">
+              <LuSun />
+            </p>
+            <p> Morning</p>
+          </div>
+        </div>
+
+        {/* Afternoon */}
+        <div className="my-6 space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
+            <p className="pt-1">
+              <LuCloudSun />
+            </p>
+            <p> Afternoon</p>
+          </div>
+        </div>
+        {/* Evening */}
+        <div className="my-6 space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
+            <p className="pt-1">
+              <GrMoon />
+            </p>
+            <p> Evening</p>
+          </div>
         </div>
       </div>
     </div>
